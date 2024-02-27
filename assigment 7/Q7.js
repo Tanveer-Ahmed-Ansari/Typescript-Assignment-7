13e6158e4034d96501d5e33cb6c4373eb8a654bb
Tanveer-Ahmed-Ansari <159915506+Tanveer-Ahmed-Ansari@users.noreply.github.com>
2024-02-27 21:53:55 +0500

    Add files via upload

---
+++ assigment 7/Q7.js
@@ -0,0 +1,12 @@
+"use strict";
+// 7.Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+let a = 4;
+let b = 4;
+// addition
+console.log(a + b);
+// subtraction
+console.log(a - b);
+// multiplication
+console.log(a * b);
+// division
+console.log(a / b);
---
+++ assigment 7/Q7.ts
@@ -0,0 +1,14 @@
+// 7.Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+
+
+let a: number = 4;
+let b: number = 4;
+// addition
+console.log (a + b)
+// subtraction
+console.log (a - b)
+// multiplication
+console.log (a * b)
+// division
+console.log (a / b)
